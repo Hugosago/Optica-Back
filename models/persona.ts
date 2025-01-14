6177ba5b377590ea7c01f8c2ddbfa6d20e4ec691
Hugosago <Hugo.sago30@gmail.com>
2025-01-14 16:13:41 -0600

    Crud Personas
    Creacion del archivo persona llamado del modelo de la tablas

---
+++ models/persona.ts
@@ -0,0 +1,52 @@
+// models/models.ts
+
+export interface Persona {
+    PersonaID: number;
+    Nombre: string;
+    Apellidos: string;
+    Contrasenia: string;
+    CorreoElectronico: string;
+  }
+
+export interface Cliente {
+  PersonaID: number;
+  edad: number;
+  ocupacion: string;
+  domicilio: string;
+  ciudad: string;
+}
+
+export interface Compra {
+  CompraID: number;
+  ClienteID: number;
+  FechaCompra: Date;
+  TotalCompra: number;
+  MetodoPago: string;
+  EstadoPago: string;
+}
+
+export interface Producto {
+  ID: number;
+  Nombre: string;
+  Descripcion: string;
+  Precio: number;
+  Imagen: string;
+}
+
+export interface Cita {
+  CitaID: number;
+  ClienteID: number;
+  Fecha_cita: string;
+  Hora_cita: string;
+  Motivo: string;
+  Estado: string;
+  Fecha_modificacion: Date;
+  comentarios: string;
+  AdminID: number;
+}
+
+
+
+export interface Admin {
+  PersonaID: number;
+}
