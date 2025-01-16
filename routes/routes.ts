8a682a790501fe93930b6e61daca83eb1a7a5d38
Hugosago <Hugo.sago30@gmail.com>
2025-01-16 17:26:31 -0600

    Crud-Productos
    Creacion del rutas del crud-productos

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "https://deno.land/x/oak@v12.1.0/mod.ts";
 import { getPersonas, createPersona, deletePersona, updatePersona } from "../controllers/persona.ts";
+import { getProductos, createProducto, updateProducto, deleteProducto, getProductoById } from "../controllers/productos.controllers.ts";
 //import { createClient, deleteClient,  } from "../controllers/client.ts";
 
 const router = new Router();
@@ -10,6 +11,12 @@ router
   .delete("/personas/:id", deletePersona)
   //.post("/clientes", createClient)
   //.delete("/clients/:id", deleteClient);
+  .get("/productos", getProductos)
+  .post("/productos", createProducto)
+  .put("/productos/:id", updateProducto)
+  .get("/productos/:id", getProductoById)
+  .delete("/productos/:id", deleteProducto);
+
 
 
 export default router;
